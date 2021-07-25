7c6c645fd3ba62fd44a2a704d1e60887d90fed84
Joaquin Farias <jfarias@lythium.cl>
2021-07-24 23:50:59 -0400

    se sube ejercicio skila terminado

--- sakila.sql
+++ sakila.sql
@@ -37,15 +37,36 @@ and (address.city_id = 1 or address.city_id = 42 or address.city_id = 312 or add
 # la película, la descripción, el año de lanzamiento, la calificación y la función especial.
 # Sugerencia: puede usar la función LIKE para obtener la parte 'detrás de escena'.
 
-
+select film.title, film.description, film.release_year, film.rating, film.special_features
+from film join film_actor on film.film_id = film_actor.film_id
+join actor on film_actor.actor_id = actor.actor_id
+where film.rating = 'G' and actor.actor_id = 15 and film.special_features like '%Behind the Scenes%';
 
 # 6. ¿Qué consulta harías para obtener todos los actores que se unieron en el film_id = 369? Su consulta debe 
 # devolver film_id, title, actor_id y actor_name.
+
+select film.film_id, film.title, actor.actor_id, concat(actor.first_name, actor.last_name) as 'actor_name' 
+from film join film_actor on film.film_id = film_actor.film_id
+join actor on actor.actor_id = film_actor.actor_id
+where film.film_id = 369;
 
 # 7. ¿Qué consulta harías para obtener todas las películas dramáticas con una tarifa de alquiler de 2.99?
 # Su consulta debe devolver el título de la película, la descripción, el año de estreno, la calificación, 
 # las características especiales y el género (categoría).
 
+select film.film_id, film.title, film.release_year, film.rating, film.special_features, category.name, film.rental_rate
+from film join film_category on film.film_id = film_category.film_id
+join category on film_category.category_id = category.category_id
+where category.name = 'Drama' and film.rental_rate = 2.99; 
+
 # 8. ¿Qué consulta harías para obtener todas las películas de acción a las que se une SANDRA KILMER? Su consulta
 # debe devolver el título de la película, la descripción, el año de estreno, la calificación, las características 
 # especiales, el género (categoría) y el nombre y apellido del actor.
+
+select actor.actor_id, concat(actor.first_name, actor.last_name) as 'actor_name', film.film_id, film.title,
+film.description, film.release_year, film.rating, film.special_features, category.name
+from film join film_actor on film.film_id = film_actor.film_id
+join actor on actor.actor_id = film_actor.actor_id
+join film_category on film.film_id = film_category.film_id
+join category on category.category_id = film_category.category_id
+where concat(actor.first_name, ' ', actor.last_name) = 'SANDRA KILMER' and category.name = 'Action';
